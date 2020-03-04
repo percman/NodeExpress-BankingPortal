61c243c448b7421be31b96f4ccac6b1891abbefc
Daniel Abramson <daniel.abramson@ruralsource.com>
2020-03-04 15:57:26 -0700

    Module 2

--- src/app.js
+++ src/app.js
@@ -8,9 +8,45 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync('src/json/accounts.json', {
+    encoding: 'UTF8'
+});
+const accounts = JSON.parse(accountData);
+
+
+const userData = fs.readFileSync('src/json/users.json', {
+    encoding: 'UTF8'
+});
+const users = JSON.parse(userData);
+
+app.get('/savings', (req, res) => {
+    res.render('account', {
+        account: accounts.savings
+    });
+});
+
+app.get('/checking', (req, res) => {
+    res.render('account', {
+        account: accounts.checking
+    });
+});
+
+app.get('/credit', (req, res) => {
+    res.render('account', {
+        account: accounts.credit
+    });
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile', {
+        user: users[0]
+    });
+});
+
 app.get('/', (req, res) => {
     res.render('index', {
-        title: 'Index'
+        title: 'Account Summary',
+        accounts: accounts
     });
 });
 
