1c0239515fdd41fd17d7f778b6fd34c5b9631033
Daniel Abramson <daniel.abramson@ruralsource.com>
2020-03-05 09:55:03 -0700

    Module 5

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const data = require('./data.js');
 const accounts = data.accounts;
 const users = data.users;
 const writeJSON = data.writeJSON;
+const accountRoutes = require('./routes/accounts.js');
+const servicesRoutes = require('./routes/services.js');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -15,63 +17,19 @@ app.use(express.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/savings', (req, res) => {
-    res.render('account', {
-        account: accounts.savings
-    });
-});
-
-app.get('/checking', (req, res) => {
-    res.render('account', {
-        account: accounts.checking
-    });
-});
-
-app.get('/credit', (req, res) => {
-    res.render('account', {
-        account: accounts.credit
-    });
-});
-
 app.get('/profile', (req, res) => {
     res.render('profile', {
         user: users[0]
     });
 });
 
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
+
 app.get('/', (req, res) => {
     res.render('index', {
         title: 'Account Summary',
         accounts: accounts
-    });
-});
-
-app.get('/transfer', (req, res) => {
-    res.render('transfer');
-});
-
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance -= req.body.amount;
-    accounts[req.body.to].balance += parseInt(req.body.amount);
-    writeJSON();
-    res.render('transfer', {
-        message: 'Transfer Completed'
-    });
-});
-
-app.get('/payment', (req, res) => {
-    res.render('payment', {
-        account: accounts.credit
-    });
-});
-
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount);
-    writeJSON();
-    res.render('payment', {
-        message: 'Payment Successful',
-        account: accounts.credit
     });
 });
 
